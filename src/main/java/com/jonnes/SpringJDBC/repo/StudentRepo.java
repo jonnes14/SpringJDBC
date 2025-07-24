67866c8de78a98e97dededb8b5b7164a3990e50e
Jonnes <jonnes400@gmail.com>
2025-07-25 01:18:13 +0530

    spring jdbctemplate

--- src/main/java/com/jonnes/SpringJDBC/repo/StudentRepo.java
+++ src/main/java/com/jonnes/SpringJDBC/repo/StudentRepo.java
@@ -1,6 +1,8 @@
 package com.jonnes.SpringJDBC.repo;
 
 import com.jonnes.SpringJDBC.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,6 +10,16 @@ import java.util.List;
 
 @Repository
 public class StudentRepo {
+   private JdbcTemplate jdbc;
+
+   @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+
     public void save(Student s){
         System.out.println("saved");
     }
@@ -16,4 +28,5 @@ public class StudentRepo {
         List<Student> students=new ArrayList<>();
         return students;
     }
+
 }
